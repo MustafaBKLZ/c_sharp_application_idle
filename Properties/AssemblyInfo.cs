6f548840d5a66f6f9dc668e3c4ff1e05beb670dc
mustafabukulmez <mustafabukulmez@hotmail.com.tr>
2021-01-25 22:29:31 +0300

    ffdg

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b4436ffe-903f-43bb-8025-d30d26596f17")]
+[assembly: Guid("fd91d9f2-1406-4263-a3e9-46c69201b266")]
 
 // Version information for an assembly consists of the following four values:
 //
